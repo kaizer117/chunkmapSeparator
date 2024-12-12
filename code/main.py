788ee0013d9b685ca521befccb6b59b3bd317a9c
kane0214 <90666199+kaizer117@users.noreply.github.com>
2024-12-13 07:04:45 +0800

    Setup home workflow

--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 img = cv.imread('./resources/example.png', cv.IMREAD_UNCHANGED)
 
 # #make mask of where the transparent bits are
@@ -7,6 +8,9 @@ img = cv.imread('./resources/example.png', cv.IMREAD_UNCHANGED)
 # #replace areas of transparency with white and not transparent
 # img[trans_mask] = [255, 255, 255, 255]
 
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
+# cv.imshow("Display window", img)
+# k = cv.waitKey(0)
+
+plt.imshow(img)
+plt.show()
 print("success")
