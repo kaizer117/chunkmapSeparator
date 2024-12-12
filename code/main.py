8345d0e77ee620d56228d923f4e196f01e0cdf19
kane0214 <90666199+kaizer117@users.noreply.github.com>
2024-12-12 17:30:51 +0800

    Populating the project folder
    Trying out opnecv2 package and also loading art resources

---
+++ code/main.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+img = cv.imread('./resources/example.png', cv.IMREAD_UNCHANGED)
+
+# #make mask of where the transparent bits are
+# trans_mask = img[:,:,3] == 0
+
+# #replace areas of transparency with white and not transparent
+# img[trans_mask] = [255, 255, 255, 255]
+
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+print("success")
