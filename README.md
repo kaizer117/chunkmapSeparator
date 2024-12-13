90771520c2231a1b1f5ad6cbbee13c114eef9d3c
kane0214 <90666199+kaizer117@users.noreply.github.com>
2024-12-13 16:24:39 +0800

    Basic Contour Mapping

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
  Map making aid for enthusiasts
 
  TODO:
- findContor() to output (x,y) at highest resolution
- multiple chubks identification and output (x,y) for each chunk
- vectorize (https://www.w3.org/TR/SVG2/paths.html#PathDataEllipticalArcCommands)
- write to svg file
- multiple chunks to separate svg files
+ findContor() to output (x,y) at highest resolution\n
+ multiple chubks identification and output (x,y) for each chunk \n
+ vectorize (https://www.w3.org/TR/SVG2/paths.html#PathDataEllipticalArcCommands)\n
+ write to svg file\n
+ multiple chunks to separate svg files\n
+
+\n\n
+https://docs.opencv.org/4.x/d9/d8b/tutorial_py_contours_hierarchy.html
+
--- code/main.py
+++ code/main.py
@@ -1,16 +1,36 @@
+
+# import packages
 import cv2 as cv
+import numpy as np
 import matplotlib.pyplot as plt
+
 img = cv.imread('./resources/example.png', cv.IMREAD_UNCHANGED)
 
-# #make mask of where the transparent bits are
-# trans_mask = img[:,:,3] == 0
+opac=cv.split(img)[3]
+hist = cv.calcHist([opac],[0],None,[256],[0,256])
 
-# #replace areas of transparency with white and not transparent
-# img[trans_mask] = [255, 255, 255, 255]
+# plotting opacity histogram
 
+# plt.plot(hist)
+# plt.title("Opacity Histogram")
+# plt.xlim([250,256])
+# plt.show()
+
+ret,thresh1 = cv.threshold(opac,253,255,cv.THRESH_BINARY)
+
+
+
+contours, hierarchy = cv.findContours(thresh1, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+# code block to replace transparent pixels with not transparent stuff.
+trans_mask = img[:,:,3] == 0
+img[trans_mask] = [255, 255, 255, 255]
+
+
+
+cv.drawContours(img,contours,0,(0,255,0),1)
+cv.imshow("contours",img)
+k = cv.waitKey(0)
+# code block to show images
 # cv.imshow("Display window", img)
-# k = cv.waitKey(0)
 
-plt.imshow(img)
-plt.show()
-print("success")
